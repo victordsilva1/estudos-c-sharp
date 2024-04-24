b8b7c4ebf0fea4711eda2caa0645f8b9ed9f4b20
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2024-04-24 12:26:31 -0300

    Update and rename if to if-big3.cs
    Bigger between three numbers.

---
+++ ifstatment/if-big3.cs
@@ -0,0 +1,17 @@
+int n1 = 10;
+int n2 = 0;
+int n3 = 9;
+if (n1>=n2 && n1>=n3)
+{
+    Console.WriteLine(n1);
+}
+else if (n2>=n1 && n2 >= n3)
+{
+    Console.WriteLine(n2);
+}
+   else 
+{
+    Console.WriteLine(n3);
+}
+
+Console.ReadLine();
